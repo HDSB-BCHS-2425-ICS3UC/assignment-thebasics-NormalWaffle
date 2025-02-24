479b20b374b871bc1fd5ae24511848da13801700
NormalWaffle <edward.natykach@gmail.com>
2025-02-24 14:31:04 -0500

    TypeWork, A.K.A. the first part of the assignment
    Tells the user about 6 different data types in computer programming,

---
+++ TypeWork.py
@@ -0,0 +1,23 @@
+#the char data type stores a single character
+u = 'j'
+print("u is a variable that stores the character", u)
+
+#the intiger data type stores a number without a decimal point
+v = 7
+print("v is an intiger, it stores the number", v, "without any decimal point")
+
+#the boolean data type stores a 1/0 (True/False)
+w = True
+print("w is a boolean, it stores either True or False (1 or 0), in this case", w)
+
+#the float data type stores a number with a decimal point, up to 7 digits
+x = 5.6
+print("x is a float, it stores a number with a decimal point, in this case", x)
+
+#the double data type stores a number with a decimal point, up to 16 digits
+y = 6.77655575555757
+print("y is also a float, but it stores more digits after the decimal point, in this case it is the number", y)
+
+#the string data type stores a string of characters, like a word or several words
+z = "Monkey"
+print("z is a string, storing the line of characters", z)
