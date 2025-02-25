d637c068fbe87beff4c56b656f416718910755cb
NormalWaffle <edward.natykach@gmail.com>
2025-02-25 14:24:04 -0500

    Math Operators / Assignment 1 part 2
    Tells the user about  a few different mathematical operations possible in python
    The comments also state what each operation does

---
+++ MathOperators.py
@@ -0,0 +1,36 @@
+#Author: Edward Natykach
+#Last Edited: 02/25/25
+#Tells the user about
+
+
+import math
+
+print("Here are some mathematical operations possible in python:")
+
+#Adds 2 numbers together
+add = 5 + 5
+print("Addition (+) adds 2 numbers together: 5 + 5 =", add)
+
+#Subtracts the second number from the first
+sub = 5-3
+print("Subtraction (-) subtracts the second number from the first: 5 - 3 =", sub)
+
+#Multiplies 2 numbers together
+mul = 4 * 3
+print("Multiplication (*) multiplies 2 numbers together: 4 * 3 =", mul)
+
+#Divides the first number by the second
+div = 10/2
+print("Division (/) divides the first number by the second: 10 / 2 =", int(div))
+
+#Gives the remains of a whole number division
+mod = 20%3
+print("Modulus (%) gives the remains of a whole number division: 20 mod 3 =", mod)
+
+#Multiplies the number by itself a given amount of times 
+pwr = 2**4
+print("Power (**) multiplies the number by itself a given amount of times: 2 ^ 4 =", pwr)
+
+#The opposite of raising a number to the power of 2 (eg 3 ^ 2 = 9, 9 sqrt = 3)
+sqrt = math.sqrt(9)
+print("Square root (sqrt) he opposite of raising a number to the power of 2: 9 sqrt =", int(sqrt))
