e38be767d2aeda6135310e97105634b288f8a0f7
NormalWaffle <edward.natykach@gmail.com>
2025-02-27 14:32:02 -0500

    Discriminant / Assignment 1 part 3
    A program that helps the user calculate the discriminant (part of the quadratic equation) of a quadratic function

---
+++ Discriminant.py
@@ -0,0 +1,11 @@
+#Author: Edward Natykach
+#Last Edited: 02/26/25
+#Helps the user calculate the discriminant of a quadratic function
+
+a = int(input("Enter a, then press Enter: ")) 
+b = int(input("Enter b, then press Enter: ")) 
+c = int(input("Enter c, then press Enter: ")) 
+#These are the input statements for a, b and c
+
+print("The Discriminant is:", str(b**2 - 4*a*c)) 
+#This calculates the discriminant and immediately prints it 
